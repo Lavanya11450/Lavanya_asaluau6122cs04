a3f57ede3571bee99be12b04dc63e80b77def77f
Lavanya11450 <144416056+Lavanya11450@users.noreply.github.com>
2023-09-21 16:07:34 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,9 @@
+English = 9.9
+Hindi = 9.0
+Maths = 10
+Science =9.9;  
+SocialStudy = 9.9
+CGPA = (9.9+9.0+10+9.9+9.9)/(5.0)  
+CGPAper = 9.5 * (CGPA)  
+print(" CGPA percentage is:");  
+print(CGPAper);
